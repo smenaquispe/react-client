d2cfac8ae019e8f9dd1979dd9376c1a3c6131d49
Sergio PC <sergiosantosmenaq@gmail.com>
2024-10-24 14:26:40 -0500

    adding button access to resources

--- src/components/Buttons/ButtonAccessToResources.jsx
+++ src/components/Buttons/ButtonAccessToResources.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
-import Resources from "./Resources";
+import Resources from "../Resources";
+import BaseButton from "./BaseButton";
 
 const ButtonAccessToResources = () => {
     const { getAccessTokenSilently, isAuthenticated } = useAuth0();
@@ -18,9 +19,9 @@ const ButtonAccessToResources = () => {
 
     return (
         <>
-            <button
+            <BaseButton
                 onClick={handleClick}
-            >Access to Resources</button>
+            >Access to Resources</BaseButton>
             {token && 
                 <>
                     <p>Token is valid</p> 
